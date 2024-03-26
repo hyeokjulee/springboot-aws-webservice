b335ba6b03f8a608d5f52d5ccc597aa9f43f7791
HyeokJu Lee <dlhjwwe@naver.com>
2024-03-26 18:41:23 +0900

    도메인 코드, 테스트 코드

---
+++ src/main/java/com/hyeokjulee/webservice/domain/Posts.java
@@ -0,0 +1,29 @@
+package com.hyeokjulee.webservice.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
