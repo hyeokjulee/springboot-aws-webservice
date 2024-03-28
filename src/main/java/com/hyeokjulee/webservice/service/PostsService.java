9685576878e6087e2014e1202a7246e536a6d839
HyeokJu Lee <dlhjwwe@naver.com>
2024-03-28 17:46:53 +0900

    3-2. 게시글 등록

---
+++ src/main/java/com/hyeokjulee/webservice/service/PostsService.java
@@ -0,0 +1,18 @@
+package com.hyeokjulee.webservice.service;
+
+import com.hyeokjulee.webservice.domain.posts.PostsRepository;
+import com.hyeokjulee.webservice.dto.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@AllArgsConstructor
+@Service
+public class PostsService {
+    private PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto dto){
+        return postsRepository.save(dto.toEntity()).getId();
+    }
+}
---
+++ src/test/java/com/hyeokjulee/webservice/service/PostServiceTest.java
@@ -0,0 +1,48 @@
+package com.hyeokjulee.webservice.service;
+
+import com.hyeokjulee.webservice.domain.posts.Posts;
+import com.hyeokjulee.webservice.domain.posts.PostsRepository;
+import com.hyeokjulee.webservice.dto.posts.PostsSaveRequestDto;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostServiceTest {
+
+    @Autowired
+    private PostsService postsService;
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup () {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void Dto데이터가_posts테이블에_저장된다 () {
+        //given
+        PostsSaveRequestDto dto = PostsSaveRequestDto.builder()
+                .author("jojoldu@gmail.com")
+                .content("테스트")
+                .title("테스트 타이틀")
+                .build();
+
+        //when
+        postsService.save(dto);
+
+        //then
+        Posts posts = postsRepository.findAll().get(0);
+        assertThat(posts.getAuthor()).isEqualTo(dto.getAuthor());
+        assertThat(posts.getContent()).isEqualTo(dto.getContent());
+        assertThat(posts.getTitle()).isEqualTo(dto.getTitle());
+    }
+}
