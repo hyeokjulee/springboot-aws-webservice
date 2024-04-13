01f01e47e2e70defe8d4ad95f2225b21331bec39
HyeokJu Lee <dlhjwwe@naver.com>
2024-04-13 16:52:28 +0900

    7-3-2. set1, set2 Profile 설정

--- src/main/java/com/hyeokjulee/webservice/web/WebRestController.java
+++ src/main/java/com/hyeokjulee/webservice/web/WebRestController.java
@@ -3,13 +3,17 @@ package com.hyeokjulee.webservice.web;
 import com.hyeokjulee.webservice.dto.PostsSaveRequestDto;
 import com.hyeokjulee.webservice.service.PostsService;
 import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
 
 @RestController
 @AllArgsConstructor
 public class WebRestController {
 
     private PostsService postsService;
+    private Environment env;
 
     @GetMapping("/hello")
     public String hello() {
@@ -19,5 +23,12 @@ public class WebRestController {
     @PostMapping("/posts")
     public Long savePosts(@RequestBody PostsSaveRequestDto dto){
         return postsService.save(dto);
+    }
+
+    @GetMapping("/profile")
+    public String getProfile () {
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
     }
 }
